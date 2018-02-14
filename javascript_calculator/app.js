9959bcde788a95f45392d8ed8e1442a641c2ad67
Steven Branson <steve@diodeware.com>
2018-02-14 12:18:33 -0600

    Add memory functionality and styling

--- javascript_calculator/app.js
+++ javascript_calculator/app.js
@@ -4,10 +4,13 @@ let currentNumber = "";
 let currentOperation = "";
 let operationStack = "";
 let displayingTotal = false;
+let restoredHistory = false;
 let calcHistory = [];
+let calcMemory = [];
 const display = document.getElementById('display-current');
 const opStackElement = document.getElementById('previous-operation-display');
 const historyElement = document.getElementById('history-list');
+const memoryElement = document.getElementById('memory-list');
 
 function toggleHistory() {
     document.getElementById('memory-container').classList.remove('h-and-m-open');
@@ -19,14 +22,16 @@ function toggleMemory() {
     document.getElementById('memory-container').classList.toggle('h-and-m-open');
 }
 
+function updateDisplays() {
+    display.innerText = currentNumber;
+    opStackElement.innerText = operationStack;
+}
+
 function finalizeOperation() {
     operationStack = operationStack + " " + currentNumber + " " + currentOperation;
-    opStackElement.innerHTML = operationStack;
-
     prevNumber = Number.parseFloat(currentNumber);
-
     currentNumber = total.toString();
-    display.innerText = currentNumber;
+    updateDisplays();
     displayingTotal = true;
 }
 
@@ -43,24 +48,38 @@ function executeOperation(op) {
 
 function numberButton(numberString) {
     if(displayingTotal || currentNumber === '0') {
-        currentNumber = "";
-        displayingTotal = false;
+        if(restoredHistory) {
+            display.innerText = "Choose an operator";
+            return;
+        } else {
+            currentNumber = "";
+            displayingTotal = false;
+        }
     }
     currentNumber += numberString;
-    display.innerText = currentNumber;
+    updateDisplays();
 }
 
 function decimal() {
     if(displayingTotal || !currentNumber) {
-        currentNumber = "0";
-        displayingTotal = false;
+        if(restoredHistory) {
+            display.innerText = "Choose an operator";
+            return;
+        } else {
+            currentNumber = "0";
+            displayingTotal = false;
+        }
     }
     currentNumber = currentNumber + ".";
-    display.innerText = currentNumber;
+    updateDisplays();
 }
 
 // some operations only change the current working value, those go here.
 function currentOp(opString) {
+    if(restoredHistory) {
+        display.innerText = "Choose an operator";
+        return;
+    }
     if(!currentNumber || currentNumber === '0') { display.innerText = "0"; currentNumber = "0"; return; }
     var n = Number.parseFloat(currentNumber);
     var m = 0;
@@ -73,12 +92,13 @@ function currentOp(opString) {
         case('backspace'): m = currentNumber.slice(0, currentNumber.length - 1); break;
     }
     currentNumber = (!!m ? m.toString() : "0");
-    display.innerText = currentNumber;
+    updateDisplays();
 }
 
 function operationButton(opString) {
     if(displayingTotal) {
-        if(!!currentOperation) {
+        if(!!currentOperation || restoredHistory) {
+            restoredHistory = false;
             swapOp(opString);
             return;
         }
@@ -93,10 +113,11 @@ function swapOp(opString) {
     currentOperation = opString;
     operationStack = operationStack.slice(0, operationStack.length - 1);
     operationStack += opString;
-    opStackElement.innerHTML = operationStack;
+    updateDisplays();
 }
 
 function equalsButton() {
+    if(restoredHistory || !currentOperation) { return; }
     executeOperation(currentOperation);
     var final = total;
     saveHistory(final);
@@ -108,7 +129,7 @@ function equalsButton() {
 
 function saveHistory(final) {
     calcHistory.push({
-        operationStack: operationStack + ' ' + currentNumber,
+        operationStack: operationStack + ' ' + currentNumber + ' =',
         total: final
     })
     rebuildHistory();
@@ -133,20 +154,140 @@ function clearState() {
     prevNumber = 0;
 }
 
+function clearHistory() {
+    calcHistory = [];
+    rebuildHistory();
+}
+
+function handleHistoryClick(e) {
+    console.log(e);
+    const el = e.target;
+    const index = el.getAttribute('data-index');
+    const o = calcHistory[index];
+    clearState();
+    restoredHistory = true;
+    operationStack = o.operationStack;
+    total = o.total;
+    currentNumber = o.total;
+    displayingTotal = true;
+    updateDisplays();
+}
+
 function rebuildHistory() {
     historyElement.innerHTML = '';
-    calcHistory.forEach(function(o){
-        let li = document.createElement('li');
-        let p1 = document.createElement('p');
-        let p2 = document.createElement('p');
+    calcHistory.forEach(function(o,i,a){
+        const li = document.createElement('li');
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+
         li.classList.add('history-item');
+        li.setAttribute('data-index', i);
+        li.addEventListener('click',handleHistoryClick);
+
         p1.classList.add('history-item-ops');
-        p1.innerText = o.operationStack + ' =';
+        p1.setAttribute('data-index', i);
+        p1.innerText = o.operationStack;
+
         p2.classList.add('history-item-total');
+        p2.setAttribute('data-index', i);
         p2.innerText = o.total;
+
         li.appendChild(p1);
         li.appendChild(p2);
-        historyElement.appendChild(li);
+        historyElement.insertBefore(li, historyElement.firstChild);
+    });
+}
+
+function clearMemory() {
+    calcMemory = [];
+    rebuildMemory();
+}
+
+function storeMemory() {
+    calcMemory.push(Number.parseFloat(currentNumber));
+    rebuildMemory();
+}
+
+function handleMemoryItemClick(e) {
+    console.log(e);
+    const i = e.target.getAttribute('data-index');
+    const func = e.target.getAttribute('data-func');
+    switch(func){
+        case('clear'): memoryClear(i); break;
+        case('add'): memoryAdd(i); break;
+        case('subtract'): memorySubtract(i); break;
+        case('recall'): memoryRecall(i); break;
+        default: break;
+    }
+}
+
+function memoryClear(i = calcMemory.length - 1) {
+    if(i === -1) { return; }
+    calcMemory.splice(i);
+    rebuildMemory();
+}
+
+function memoryAdd(i = calcMemory.length - 1) {
+    if(i === -1) { i = 0; }
+    calcMemory[i] += Number.parseFloat(currentNumber);
+    rebuildMemory();
+}
+
+function memorySubtract(i = calcMemory.length - 1) {
+    if(i === -1) { i = 0; }
+    calcMemory[i] -= Number.parseFloat(currentNumber);
+    rebuildMemory();
+}
+
+function memoryRecall(i = calcMemory.length - 1) {
+    if(i === -1) { return; }
+    clearState();
+    currentNumber = "" + calcMemory[i];
+    updateDisplays();
+}
+
+function rebuildMemory() {
+    memoryElement.innerHTML = '';
+    calcMemory.forEach(function(v,i,a){
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        const b1 = document.createElement('button');
+        const b2 = document.createElement('button');
+        const b3 = document.createElement('button');
+
+        li.classList.add('memory-item');
+        li.setAttribute('data-index', i);
+        li.setAttribute('data-func', 'recall');
+        li.addEventListener('click', handleMemoryItemClick);
+
+        p.classList.add('memory-item-value');
+        p.setAttribute('data-index', i);
+        p.setAttribute('data-func', 'recall');
+        p.innerText = v;
+
+        b1.classList.add('memory-item-button');
+        b1.classList.add('memory-item-clear');
+        b1.setAttribute('data-index', i);
+        b1.setAttribute('data-func', 'clear');
+        b1.innerText = 'MC';
+
+        b2.classList.add('memory-item-button');
+        b2.classList.add('memory-item-add');
+        b2.setAttribute('data-index', i);
+        b2.setAttribute('data-func', 'add');
+        b2.innerText = 'M+';    
+
+        b3.classList.add('memory-item-button');
+        b3.classList.add('memory-item-subtract');
+        b3.setAttribute('data-index', i);
+        b3.setAttribute('data-func', 'subtract');
+        b3.innerText = 'M-';
+
+        li.appendChild(p);
+        li.appendChild(b1);
+        li.appendChild(b2);
+        li.appendChild(b3);
+        memoryElement.insertBefore(li, memoryElement.firstChild);
     });
 }
 
