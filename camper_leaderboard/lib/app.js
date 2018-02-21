980481d0fe77c62ce12b63e9241b1c31331fc493
Steven Branson <steve@diodeware.com>
2018-02-21 12:55:15 -0600

    Draw rest of owl

--- camper_leaderboard/lib/app.js
+++ camper_leaderboard/lib/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -8,85 +8,84 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var MINIMUM_FETCH_TIME = 1000000;
+
 function TableRow(props) {
     return React.createElement(
-        'tr',
+        "tr",
         null,
         React.createElement(
-            'td',
+            "td",
             null,
             props.ranking
         ),
         React.createElement(
-            'td',
-            null,
-            props.camper.name
+            "td",
+            { className: "camper-name-cell" },
+            React.createElement("img", { src: props.camper.img, className: "avatarpic" }),
+            React.createElement(
+                "a",
+                { href: 'https://www.freecodecamp.com/' + props.camper.username },
+                props.camper.username
+            )
         ),
         React.createElement(
-            'td',
+            "td",
             null,
-            props.camper.month_points
+            props.camper.recent
         ),
         React.createElement(
-            'td',
+            "td",
             null,
-            props.camper.all_points
+            props.camper.alltime
         )
     );
 }
 
 function Table(props) {
-    var pred = function pred() {
-        return 1;
-    };
-    switch (props.sort) {
-        case 'all':
-            pred = function pred(a, b) {
-                return a.all_points < b.all_points ? 1 : -1;
-            };break;
-        case '30':
-            pred = function pred(a, b) {
-                return a.month_points < b.month_points ? 1 : -1;
-            };break;
-    }
-    var tableRows = props.campers.sort(pred).map(function (e, i) {
-        return React.createElement(TableRow, { key: e.id, ranking: i + 1, camper: e });
+    // let pred = function() { return 1 };
+    // switch(props.sort) {
+    //     case('all'): pred = function(a, b) { return (a.all_points < b.all_points) ? 1 : -1; } ; break;
+    //     case('30'): pred = function(a, b) { return (a.month_points < b.month_points) ? 1 : -1; }; break;
+    // }
+    var tableRows = props.campers.map(function (e, i) {
+        return React.createElement(TableRow, { key: i, ranking: i + 1, camper: e });
     });
     return React.createElement(
-        'table',
-        null,
+        "table",
+        { className: "main-table" },
         React.createElement(
-            'thead',
+            "thead",
             null,
             React.createElement(
-                'tr',
+                "tr",
                 null,
                 React.createElement(
-                    'th',
+                    "th",
                     null,
-                    'Rank'
+                    "Rank"
                 ),
                 React.createElement(
-                    'th',
+                    "th",
                     null,
-                    'Name'
+                    "Name"
                 ),
                 React.createElement(
-                    'th',
+                    "th",
                     { className: props.sort == '30' ? 'selected' : 'selectable',
                         onClick: props.sort_30 },
-                    'Last 30 Days'
+                    "Last 30 Days"
                 ),
                 React.createElement(
-                    'th',
+                    "th",
                     { className: props.sort == 'all' ? 'selected' : 'selectable',
                         onClick: props.sort_all },
-                    'All Time'
+                    "All Time"
                 )
             )
         ),
         React.createElement(
-            'tbody',
+            "tbody",
             null,
             tableRows
         )
@@ -95,9 +94,13 @@ function Table(props) {
 
 function TopBar(props) {
     return React.createElement(
-        'h1',
-        null,
-        'Top Bar'
+        "div",
+        { id: "app-title" },
+        React.createElement(
+            "h1",
+            { id: "app-title-text" },
+            "freeCodeCamp Leaderboards"
+        )
     );
 }
 
@@ -110,38 +113,70 @@ var App = function (_React$Component) {
         var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
         _this.state = {
-            campers: [{
-                id: 0,
-                name: 'Steven B',
-                month_points: 1,
-                all_points: 3
-            }, {
-                id: 1,
-                name: 'Kitty H',
-                month_points: 2,
-                all_points: 2
-            }],
-            sort: 'all'
+            sort: 'all',
+            last_fetch_time_all: 0,
+            last_fetch_time_30: 0,
+            campers_30: [{}],
+            campers_all: [{
+                username: "errpr",
+                img: "https://avatars3.githubusercontent.com/u/22058959?v=1",
+                alltime: 3,
+                recent: 1,
+                lastUpdate: "2018-02-20T19:58:50.041Z"
+            }]
+        };
+
+        _this.fetch_top_all = function () {
+            if (Date.now() - _this.state.last_fetch_time_all > MINIMUM_FETCH_TIME) {
+                fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime', { mode: 'cors', headers: { 'accept': 'application/json' } }).then(function (response) {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                }).then(function (json) {
+                    return _this.setState({ campers_all: json, last_fetch_time_all: Date.now });
+                });
+            }
+        };
+
+        _this.fetch_top_30 = function () {
+            if (Date.now() - _this.state.last_fetch_time_30 > MINIMUM_FETCH_TIME) {
+                fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent', { mode: 'cors', headers: { 'accept': 'application/json' } }).then(function (response) {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                }).then(function (json) {
+                    return _this.setState({ campers_30: json, last_fetch_time_30: Date.now });
+                });
+            }
         };
 
         _this.sort_all = function () {
+            _this.fetch_top_all();
             _this.setState({ sort: 'all' });
         };
+
         _this.sort_30 = function () {
+            _this.fetch_top_30();
             _this.setState({ sort: '30' });
         };
         return _this;
     }
 
     _createClass(App, [{
-        key: 'render',
+        key: "componentDidMount",
+        value: function componentDidMount() {
+            this.fetch_top_all();
+            this.fetch_top_30();
+        }
+    }, {
+        key: "render",
         value: function render() {
             return React.createElement(
-                'div',
-                { id: 'app' },
+                "div",
+                { id: "app" },
                 React.createElement(TopBar, null),
                 React.createElement(Table, {
-                    campers: this.state.campers,
+                    campers: this.state.sort == 'all' ? this.state.campers_all : this.state.campers_30,
                     sort_30: this.sort_30,
                     sort_all: this.sort_all,
                     sort: this.state.sort })
--- camper_leaderboard/src/app.jsx
+++ camper_leaderboard/src/app.jsx
@@ -1,25 +1,32 @@
+const MINIMUM_FETCH_TIME = 1000000;
+
 function TableRow(props) {
     return(
         <tr>
             <td>{props.ranking}</td>
-            <td>{props.camper.name}</td>
-            <td>{props.camper.month_points}</td>
-            <td>{props.camper.all_points}</td>
+            <td className="camper-name-cell">
+                <img src={props.camper.img} className="avatarpic" />
+                <a href={'https://www.freecodecamp.com/' + props.camper.username}>
+                    {props.camper.username}
+                </a>
+            </td>
+            <td>{props.camper.recent}</td>
+            <td>{props.camper.alltime}</td>
         </tr>
     );
 }
 
 function Table(props) {
-    let pred = function() { return 1 };
-    switch(props.sort) {
-        case('all'): pred = function(a, b) { return (a.all_points < b.all_points) ? 1 : -1; } ; break;
-        case('30'): pred = function(a, b) { return (a.month_points < b.month_points) ? 1 : -1; }; break;
-    }
-    let tableRows = props.campers
-                         .sort(pred)
-                         .map((e, i) => <TableRow key={e.id} ranking={i+1} camper={e} />);
+    // let pred = function() { return 1 };
+    // switch(props.sort) {
+    //     case('all'): pred = function(a, b) { return (a.all_points < b.all_points) ? 1 : -1; } ; break;
+    //     case('30'): pred = function(a, b) { return (a.month_points < b.month_points) ? 1 : -1; }; break;
+    // }
+    let tableRows = props
+                    .campers
+                    .map((e, i) => <TableRow key={i} ranking={i+1} camper={e} />);
     return(
-        <table>
+        <table className="main-table">
             <thead>
                 <tr>
                     <th>Rank</th>
@@ -38,41 +45,74 @@ function Table(props) {
 }
 
 function TopBar(props) {
-    return(<h1>Top Bar</h1>);
+    return(
+        <div id="app-title">
+            <h1 id="app-title-text">freeCodeCamp Leaderboards</h1>
+        </div>
+        );
 }
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            campers: [{
-                id: 0,
-                name: 'Steven B',
-                month_points: 1,
-                all_points: 3
-            },
-            {
-                id: 1,
-                name: 'Kitty H',
-                month_points: 2,
-                all_points: 2
+            sort: 'all',
+            last_fetch_time_all: 0,
+            last_fetch_time_30: 0,
+            campers_30: [{}],
+            campers_all: [{
+                username: "errpr",
+                img: "https://avatars3.githubusercontent.com/u/22058959?v=1",
+                alltime: 3,
+                recent: 1,
+                lastUpdate: "2018-02-20T19:58:50.041Z"
             }],
-            sort: 'all'
         };
 
+        this.fetch_top_all = () => {
+            if(Date.now() - this.state.last_fetch_time_all > MINIMUM_FETCH_TIME) {
+                fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime', 
+                      { mode: 'cors', headers: { 'accept' : 'application/json' } }).then(response => {
+                    if(response.ok) {
+                        return(response.json());
+                    }
+                }).then(json => this.setState({ campers_all: json, last_fetch_time_all: Date.now }))
+            }
+        }
+
+        this.fetch_top_30 = () => {
+            if(Date.now() - this.state.last_fetch_time_30 > MINIMUM_FETCH_TIME) {
+                fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent', 
+                      { mode: 'cors', headers: { 'accept' : 'application/json' } }).then(response => {
+                    if(response.ok) {
+                        return(response.json());
+                    }
+                }).then(json => this.setState({ campers_30: json, last_fetch_time_30: Date.now }))
+            }
+        }
+
         this.sort_all = () => {
+            this.fetch_top_all();
             this.setState({ sort: 'all' });
         }
+
         this.sort_30 = () => {
+            this.fetch_top_30();
             this.setState({ sort: '30' });
         }
     }
+
+    componentDidMount() {
+        this.fetch_top_all();
+        this.fetch_top_30();
+    }
+
     render() {
         return(
             <div id="app">
                 <TopBar />
                 <Table 
-                    campers={this.state.campers}
+                    campers={this.state.sort == 'all' ? this.state.campers_all : this.state.campers_30}
                     sort_30={this.sort_30}
                     sort_all={this.sort_all}
                     sort={this.state.sort} />
