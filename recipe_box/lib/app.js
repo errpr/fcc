38820a41a45281c6d35cfb7e87efcb0f2304bfe2
Steven Branson <steve@diodeware.com>
2018-02-21 14:48:34 -0600

    Create recipe_box app skeleton

--- recipe_box/lib/app.js
+++ recipe_box/lib/app.js
@@ -8,22 +8,84 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var App = function (_React$Component) {
-    _inherits(App, _React$Component);
+function RecipeList(props) {
+    var recipeItems = props.recipes.map(function (e, i) {
+        return React.createElement(
+            'li',
+            { onClick: props.select,
+                'data-index': i,
+                key: i },
+            e.name
+        );
+    });
+    return React.createElement(
+        'ul',
+        null,
+        recipeItems
+    );
+}
 
-    function App() {
+var CurrentRecipe = function (_React$Component) {
+    _inherits(CurrentRecipe, _React$Component);
+
+    function CurrentRecipe() {
+        _classCallCheck(this, CurrentRecipe);
+
+        return _possibleConstructorReturn(this, (CurrentRecipe.__proto__ || Object.getPrototypeOf(CurrentRecipe)).apply(this, arguments));
+    }
+
+    _createClass(CurrentRecipe, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                JSON.stringify(this.props.recipe),
+                ')'
+            );
+        }
+    }]);
+
+    return CurrentRecipe;
+}(React.Component);
+
+var App = function (_React$Component2) {
+    _inherits(App, _React$Component2);
+
+    function App(props) {
         _classCallCheck(this, App);
 
-        return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+        var _this2 = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+
+        _this2.state = {
+            recipes: [{
+                name: 'Steven\'s Red Beans & Rice',
+                ingredients: [{ amount: '2 cans', item: 'red beans' }, { amount: '2 packages', item: 'Uncle Ben\'s 90 second white rice' }, { amount: '1 package', item: 'smoked sausage' }, { amount: '3/4', item: 'onion diced' }, { amount: '3/4', item: 'bell pepper diced' }, { amount: '3 dashes', item: 'Tony\'s Cajun Seasoning' }]
+            }],
+            selectedRecipe: 0
+        };
+
+        _this2.selectRecipeHandler = function (e) {
+            _this2.setState({ selectedRecipe: e.target.getAttribute('data-index') });
+        };
+        return _this2;
     }
 
     _createClass(App, [{
         key: 'render',
         value: function render() {
             return React.createElement(
-                'h1',
-                null,
-                'App'
+                'div',
+                { id: 'app-container' },
+                React.createElement(RecipeList, {
+                    recipes: this.state.recipes,
+                    select: this.selectRecipeHandler }),
+                React.createElement(CurrentRecipe, { recipe: this.state.recipes[this.state.selectedRecipe] }),
+                React.createElement(
+                    'pre',
+                    null,
+                    JSON.stringify(this.state, null, 2)
+                )
             );
         }
     }]);
--- recipe_box/src/app.jsx
+++ recipe_box/src/app.jsx
@@ -1,6 +1,57 @@
-class App extends React.Component {
+function RecipeList(props) {
+    let recipeItems = props.recipes.map((e, i) => <li onClick={props.select}
+                                                      data-index={i} 
+                                                      key={i}>{e.name}</li>);
+    return(
+        <ul>
+            {recipeItems}
+        </ul>
+    );
+}
+
+class CurrentRecipe extends React.Component {
     render() {
-        return(<h1>App</h1>);
+        return(
+            <div>{JSON.stringify(this.props.recipe)})</div>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipes: [
+                {
+                    name: 'Steven\'s Red Beans & Rice',
+                    ingredients: [
+                        { amount: '2 cans',     item: 'red beans'},
+                        { amount: '2 packages', item: 'Uncle Ben\'s 90 second white rice' },
+                        { amount: '1 package',  item: 'smoked sausage' },
+                        { amount: '3/4',        item: 'onion diced' },
+                        { amount: '3/4',        item: 'bell pepper diced' },
+                        { amount: '3 dashes',   item: 'Tony\'s Cajun Seasoning' }
+                    ]
+                }
+            ],
+            selectedRecipe: 0
+        }
+
+        this.selectRecipeHandler = (e) => {
+            this.setState({ selectedRecipe: e.target.getAttribute('data-index') });
+        }
+    }
+
+    render() {
+        return(
+            <div id="app-container">
+                <RecipeList 
+                    recipes={this.state.recipes} 
+                    select={this.selectRecipeHandler} />
+                <CurrentRecipe recipe={this.state.recipes[this.state.selectedRecipe]} />
+                <pre>{JSON.stringify(this.state, null, 2)}</pre>
+            </div>
+        );
     }
 }
 ReactDOM.render(<App />, document.getElementById('root'));
