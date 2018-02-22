65c95c4651fab54419133f6bd90b02c250dfbce0
Steven Branson <steve@diodeware.com>
2018-02-22 17:45:59 -0600

    Style recipe list, fix wonkyness with editing recipe ingredients, add editing recipe names

--- recipe_box/lib/app.js
+++ recipe_box/lib/app.js
@@ -10,6 +10,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+//@ts-check
 function RecipeList(props) {
     var recipeItems = props.recipes.map(function (e, i) {
         return React.createElement(
@@ -21,12 +22,28 @@ function RecipeList(props) {
             e.name
         );
     });
+    var emptyLine = React.createElement("li", { className: "recipe-item", onClick: props.addRecipe });
+    for (var i = 0; i < 30 - recipeItems.length; i++) {
+        recipeItems.push(emptyLine);
+    }
     return React.createElement(
         "ul",
-        { className: "recipe-list" },
-        recipeItems
+        { className: "recipe-list", onClick: props.handleClick },
+        React.createElement(
+            "li",
+            { className: "recipe-item" },
+            React.createElement(
+                "h1",
+                null,
+                "Recipes"
+            )
+        ),
+        recipeItems,
+        emptyLine
     );
 }
+
+// @ts-ignore
 
 var IngredientLine = function (_React$Component) {
     _inherits(IngredientLine, _React$Component);
@@ -62,7 +79,7 @@ var IngredientLine = function (_React$Component) {
         _this.handleChange = function (e) {
             if (e.target.name === "edit-amount") {
                 _this.setState({ editingAmountValue: e.target.value });
-            } else {
+            } else if (e.target.name === "edit-item") {
                 _this.setState({ editingItemValue: e.target.value });
             }
         };
@@ -71,7 +88,7 @@ var IngredientLine = function (_React$Component) {
             if (e.target.name === "edit-amount") {
                 _this.updateAmount(_this.state.editingAmountValue);
                 _this.setState({ editingAmount: false });
-            } else {
+            } else if (e.target.name === "edit-item") {
                 _this.updateItem(_this.state.editingItemValue);
                 _this.setState({ editingItem: false });
             }
@@ -87,14 +104,15 @@ var IngredientLine = function (_React$Component) {
                 { className: "recipe-ingredient" },
                 React.createElement(
                     "div",
-                    { className: "ingredient-amount" + (!this.state.editingAmount ? " visible" : " invisible"),
+                    { className: "ingredient-amount" + (!this.state.editingAmount ? "" : " invisible"),
                         onDoubleClick: this.dblclickAmount },
                     this.props.amount
                 ),
                 React.createElement(
                     "div",
-                    { className: "ingredient-amount" + (this.state.editingAmount ? " visible" : " invisible") },
+                    { className: "ingredient-amount" + (this.state.editingAmount ? "" : " invisible") },
                     React.createElement("input", { name: "edit-amount",
+                        type: "text",
                         className: "edit-amount",
                         onChange: this.handleChange,
                         onBlur: this.handleBlur,
@@ -102,14 +120,15 @@ var IngredientLine = function (_React$Component) {
                 ),
                 React.createElement(
                     "div",
-                    { className: "ingredient-item" + (!this.state.editingItem ? " visible" : " invisible"),
+                    { className: "ingredient-item" + (!this.state.editingItem ? "" : " invisible"),
                         onDoubleClick: this.dblclickItem },
                     this.props.item
                 ),
                 React.createElement(
                     "div",
-                    { className: "ingredient-item" + (this.state.editingItem ? " visible" : " invisible") },
+                    { className: "ingredient-item" + (this.state.editingItem ? "" : " invisible") },
                     React.createElement("input", { name: "edit-item",
+                        type: "text",
                         className: "edit-item",
                         onChange: this.handleChange,
                         onBlur: this.handleBlur,
@@ -122,6 +141,9 @@ var IngredientLine = function (_React$Component) {
     return IngredientLine;
 }(React.Component);
 
+// @ts-ignore
+
+
 var CurrentRecipe = function (_React$Component2) {
     _inherits(CurrentRecipe, _React$Component2);
 
@@ -129,6 +151,21 @@ var CurrentRecipe = function (_React$Component2) {
         _classCallCheck(this, CurrentRecipe);
 
         var _this2 = _possibleConstructorReturn(this, (CurrentRecipe.__proto__ || Object.getPrototypeOf(CurrentRecipe)).call(this, props));
+
+        _this2.state = {
+            nameValue: props.recipe.name,
+            editingName: false
+        };
+
+        _this2.updateName = function (newName) {
+            if (!newName) {
+                _this2.setState({ nameValue: props.recipe.name });
+                return;
+            }
+            _this2.props.updateRecipe(_extends({}, _this2.props.recipe, {
+                name: newName
+            }));
+        };
 
         _this2.updateIngredient = function (ingredientIndex) {
             return function (amount, item) {
@@ -139,6 +176,19 @@ var CurrentRecipe = function (_React$Component2) {
                 }));
             }.bind(_this2);
         };
+
+        _this2.handleNameDblClick = function (e) {
+            _this2.setState({ editingName: true });
+        };
+
+        _this2.handleNameChange = function (e) {
+            _this2.setState({ nameValue: e.target.value });
+        };
+
+        _this2.handleNameBlur = function (e) {
+            _this2.setState({ editingName: false });
+            _this2.updateName(_this2.state.nameValue);
+        };
         return _this2;
     }
 
@@ -148,19 +198,25 @@ var CurrentRecipe = function (_React$Component2) {
             var _this3 = this;
 
             var ingredients = this.props.recipe.ingredients.map(function (e, i) {
-                return React.createElement(IngredientLine, { key: i,
+                return React.createElement(IngredientLine, { key: _this3.props.recipe.name + i,
                     amount: e.amount,
                     item: e.item,
                     updateIngredient: _this3.updateIngredient(i) });
             });
             return React.createElement(
                 "div",
-                { className: "recipe-card" },
+                { className: "recipe-card" + (this.props.visible ? '' : ' invisible') },
                 React.createElement(
                     "h3",
-                    { className: "recipe-name" },
+                    { className: "recipe-name" + (!this.state.editingName ? '' : ' invisible'),
+                        onDoubleClick: this.handleNameDblClick },
                     this.props.recipe.name
                 ),
+                React.createElement("input", { className: "recipe-name" + (this.state.editingName ? '' : ' invisible'),
+                    type: "text",
+                    onChange: this.handleNameChange,
+                    onBlur: this.handleNameBlur,
+                    value: this.state.nameValue }),
                 React.createElement(
                     "div",
                     null,
@@ -179,6 +235,9 @@ var CurrentRecipe = function (_React$Component2) {
     return CurrentRecipe;
 }(React.Component);
 
+// @ts-ignore
+
+
 var App = function (_React$Component3) {
     _inherits(App, _React$Component3);
 
@@ -190,16 +249,17 @@ var App = function (_React$Component3) {
         _this4.state = {
             recipes: [{
                 name: 'Steven\'s Red Beans & Rice',
-                ingredients: [{ amount: '2 cans', item: 'red beans' }, { amount: '2 packages', item: 'Uncle Ben\'s 90 second white rice' }, { amount: '1 package', item: 'smoked sausage' }, { amount: '3/4', item: 'onion diced' }, { amount: '3/4', item: 'bell pepper diced' }, { amount: '3 dashes', item: 'Tony\'s Cajun Seasoning' }]
+                ingredients: [{ amount: '2 cans', item: 'red beans' }, { amount: '2 packages', item: 'Uncle Ben\'s 90 second white rice' }, { amount: '1 package', item: 'smoked sausage' }, { amount: '3/4', item: 'onion diced' }, { amount: '3/4', item: 'bell pepper diced' }, { amount: '3 dashes', item: 'Tony\'s Cajun Seasoning' }, { amount: '1 tblsp', item: 'chili pepper powdered' }, { amount: '1 tblsp', item: 'cayenne pepper powdered' }]
             }, {
                 name: 'Steven\'s Mac n Cheese',
-                ingredients: [{ amount: '1 package', item: 'macaroni pasta' }, { amount: '1 package', item: 'cream cheese' }, { amount: '1/2 package', item: 'sharp cheddar' }, { amount: '1/2 package', item: 'mild cheddar' }, { amount: '2 tblsp', item: 'salt' }, { amount: '1/8 gallon', item: 'milk' }]
+                ingredients: [{ amount: '1 package', item: 'macaroni pasta' }, { amount: '1 package', item: 'cream cheese' }, { amount: '1/2 package', item: 'sharp cheddar shredded' }, { amount: '1/2 package', item: 'mild cheddar shredded' }, { amount: '2 tblsp', item: 'salt' }, { amount: '1/8 gallon', item: 'milk' }]
             }],
-            selectedRecipe: 0
+            selectedRecipe: 0,
+            cardVisible: false
         };
 
         _this4.selectRecipeHandler = function (e) {
-            _this4.setState({ selectedRecipe: e.target.getAttribute('data-index') });
+            _this4.setState({ selectedRecipe: e.target.getAttribute('data-index'), cardVisible: true });
         };
 
         _this4.updateRecipe = function (recipeIndex) {
@@ -210,6 +270,12 @@ var App = function (_React$Component3) {
                     return _extends({}, prevState, { recipes: new_recipes });
                 });
             }.bind(_this4);
+        };
+
+        _this4.handleRecipeListClick = function (e) {
+            if (_this4.state.cardVisible) {
+                _this4.setState({ cardVisible: false });
+            }
         };
         return _this4;
     }
@@ -222,8 +288,10 @@ var App = function (_React$Component3) {
                 { id: "app-container" },
                 React.createElement(RecipeList, {
                     recipes: this.state.recipes,
-                    select: this.selectRecipeHandler }),
+                    select: this.selectRecipeHandler,
+                    handleClick: this.handleRecipeListClick }),
                 React.createElement(CurrentRecipe, {
+                    visible: this.state.cardVisible,
                     recipe: this.state.recipes[this.state.selectedRecipe],
                     updateRecipe: this.updateRecipe(this.state.selectedRecipe) })
             );
@@ -232,5 +300,7 @@ var App = function (_React$Component3) {
 
     return App;
 }(React.Component);
+// @ts-ignore
+
 
 ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
--- recipe_box/src/app.jsx
+++ recipe_box/src/app.jsx
@@ -1,15 +1,23 @@
+//@ts-check
 function RecipeList(props) {
     let recipeItems = props.recipes.map((e, i) => <li className="recipe-item"
                                                       onClick={props.select}
                                                       data-index={i} 
                                                       key={i}>{e.name}</li>);
+    let emptyLine = <li className="recipe-item" onClick={props.addRecipe}></li>;
+    for(let i = 0; i < 30 - recipeItems.length; i++){
+        recipeItems.push(emptyLine);
+    }
     return(
-        <ul className="recipe-list">
+        <ul className="recipe-list" onClick={props.handleClick}>
+            <li className="recipe-item"><h1>Recipes</h1></li>
             {recipeItems}
+            {emptyLine}
         </ul>
     );
 }
 
+// @ts-ignore
 class IngredientLine extends React.Component {
     constructor(props) {
         super(props);
@@ -39,7 +47,7 @@ class IngredientLine extends React.Component {
         this.handleChange = (e) => {
             if(e.target.name === "edit-amount") {
                 this.setState({ editingAmountValue: e.target.value });
-            } else {
+            } else if(e.target.name === "edit-item" ) {
                 this.setState({ editingItemValue: e.target.value });
             }
         }
@@ -48,7 +56,7 @@ class IngredientLine extends React.Component {
             if(e.target.name === "edit-amount") {
                 this.updateAmount(this.state.editingAmountValue);
                 this.setState({ editingAmount: false });
-            } else {
+            } else if(e.target.name === "edit-item" ) {
                 this.updateItem(this.state.editingItemValue);
                 this.setState({ editingItem: false });
             }
@@ -60,24 +68,26 @@ class IngredientLine extends React.Component {
     render() {
         return(
             <div className="recipe-ingredient">
-                <div className={"ingredient-amount" + (!this.state.editingAmount ? " visible" : " invisible")} 
+                <div className={"ingredient-amount" + (!this.state.editingAmount ? "" : " invisible")} 
                      onDoubleClick={this.dblclickAmount}>
                      {this.props.amount}
                 </div>
-                <div className={"ingredient-amount" + (this.state.editingAmount ? " visible" : " invisible")}>
+                <div className={"ingredient-amount" + (this.state.editingAmount ? "" : " invisible")}>
                     <input name="edit-amount" 
-                            className="edit-amount"
-                            onChange={this.handleChange} 
-                            onBlur={this.handleBlur} 
-                            value={this.state.editingAmountValue} />
+                           type="text"
+                           className="edit-amount"
+                           onChange={this.handleChange} 
+                           onBlur={this.handleBlur} 
+                           value={this.state.editingAmountValue} />
                 </div>
 
-                <div className={"ingredient-item" + (!this.state.editingItem ? " visible" : " invisible")}
+                <div className={"ingredient-item" + (!this.state.editingItem ? "" : " invisible")}
                      onDoubleClick={this.dblclickItem}>
                      {this.props.item}
                 </div>
-                <div className={"ingredient-item" + (this.state.editingItem ? " visible" : " invisible")}>
-                    <input name="edit-item" 
+                <div className={"ingredient-item" + (this.state.editingItem ? "" : " invisible")}>
+                    <input name="edit-item"
+                           type="text"
                            className="edit-item"  
                            onChange={this.handleChange} 
                            onBlur={this.handleBlur}
@@ -88,9 +98,26 @@ class IngredientLine extends React.Component {
     }
 }
 
+// @ts-ignore
 class CurrentRecipe extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            nameValue: props.recipe.name,
+            editingName: false
+        }
+
+        this.updateName = (newName) => {
+            if(!newName) {
+                this.setState({ nameValue: props.recipe.name });
+                return;
+            }
+            this.props.updateRecipe({
+                ...this.props.recipe,
+                name: newName
+            })
+        }
 
         this.updateIngredient = (ingredientIndex) => {
             return function(amount, item) {
@@ -102,22 +129,42 @@ class CurrentRecipe extends React.Component {
                 })
             }.bind(this);
         }
+
+        this.handleNameDblClick = (e) => {
+            this.setState({ editingName: true });
+        }
+
+        this.handleNameChange = (e) => {
+            this.setState({ nameValue: e.target.value });
+        }
+
+        this.handleNameBlur = (e) => {
+            this.setState({ editingName: false });
+            this.updateName(this.state.nameValue);
+        }
     }
 
     render() {
         let ingredients = this.props
                               .recipe
                               .ingredients
-                              .map((e, i) => { 
-                                  return(<IngredientLine key={i}
-                                                         amount={e.amount}
-                                                         item={e.item} 
-                                                         updateIngredient={this.updateIngredient(i)} />); });
+                              .map((e, i) => { return(
+                                <IngredientLine key={this.props.recipe.name + i}
+                                                amount={e.amount}
+                                                item={e.item} 
+                                                updateIngredient={this.updateIngredient(i)} />
+                              ); });
         return(
-            <div className="recipe-card">
-                <h3 className="recipe-name">
+            <div className={"recipe-card" + (this.props.visible ? '' : ' invisible')}>
+                <h3 className={"recipe-name" + (!this.state.editingName ? '' : ' invisible')}
+                    onDoubleClick={this.handleNameDblClick}>
                     {this.props.recipe.name}
                 </h3>
+                <input  className={"recipe-name" + (this.state.editingName ? '' : ' invisible')}
+                        type="text" 
+                        onChange={this.handleNameChange}
+                        onBlur={this.handleNameBlur} 
+                        value={this.state.nameValue} />
                 <div>
                     {ingredients}
                     <div className="recipe-ingredient">
@@ -130,6 +177,7 @@ class CurrentRecipe extends React.Component {
     }
 }
 
+// @ts-ignore
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -143,25 +191,28 @@ class App extends React.Component {
                         { amount: '1 package',  item: 'smoked sausage' },
                         { amount: '3/4',        item: 'onion diced' },
                         { amount: '3/4',        item: 'bell pepper diced' },
-                        { amount: '3 dashes',   item: 'Tony\'s Cajun Seasoning' }
+                        { amount: '3 dashes',   item: 'Tony\'s Cajun Seasoning' },
+                        { amount: '1 tblsp',    item: 'chili pepper powdered'},
+                        { amount: '1 tblsp',    item: 'cayenne pepper powdered'}
                     ]
                 },{
                     name: 'Steven\'s Mac n Cheese',
                     ingredients: [
                         { amount: '1 package',  item: 'macaroni pasta'},
                         { amount: '1 package',  item: 'cream cheese' },
-                        { amount: '1/2 package',item: 'sharp cheddar' },
-                        { amount: '1/2 package',item: 'mild cheddar' },
+                        { amount: '1/2 package',item: 'sharp cheddar shredded' },
+                        { amount: '1/2 package',item: 'mild cheddar shredded' },
                         { amount: '2 tblsp',    item: 'salt' },
                         { amount: '1/8 gallon', item: 'milk' },
                     ]
                 },
             ],
-            selectedRecipe: 0
+            selectedRecipe: 0,
+            cardVisible: false
         }
 
         this.selectRecipeHandler = (e) => {
-            this.setState({ selectedRecipe: e.target.getAttribute('data-index') });
+            this.setState({ selectedRecipe: e.target.getAttribute('data-index'), cardVisible: true });
         }
 
         this.updateRecipe = (recipeIndex) => {
@@ -173,6 +224,12 @@ class App extends React.Component {
                 });
             }.bind(this);
         }
+
+        this.handleRecipeListClick = (e) => {
+            if(this.state.cardVisible) {
+                this.setState({cardVisible: false});
+            }
+        }
     }
 
     
@@ -182,12 +239,15 @@ class App extends React.Component {
             <div id="app-container">
                 <RecipeList 
                     recipes={this.state.recipes} 
-                    select={this.selectRecipeHandler} />
+                    select={this.selectRecipeHandler}
+                    handleClick={this.handleRecipeListClick} />
                 <CurrentRecipe 
+                    visible={this.state.cardVisible}
                     recipe={this.state.recipes[this.state.selectedRecipe]}
                     updateRecipe={this.updateRecipe(this.state.selectedRecipe)} />
             </div>
         );
     }
 }
+// @ts-ignore
 ReactDOM.render(<App />, document.getElementById('root'));
