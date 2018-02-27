56559d3d0c8f8620d5a2723d3b3b52618a56a9ae
Steven Branson <steve@diodeware.com>
2018-02-27 12:03:41 -0600

    Remove debugging console.logs

--- dungeon_crawler/src/app.jsx
+++ dungeon_crawler/src/app.jsx
@@ -148,7 +148,6 @@ function resetGlobals() {
 resetGlobals();
 
 function randomEnemy() {
-    console.log("spawn");
     let value = Math.floor(Math.random() * NUMBER_OF_ENEMY_ENTITIES) + NUMBER_OF_ITEM_DROP_ENTITIES;
     return new Entity(value);
 }
@@ -165,7 +164,6 @@ function generateRoomEntities(room) {
                 (i == 1 && j == room.midWidth) ||
                 (i == room.height - 2 && j == room.midWidth) ){
                 //dont spawn enemy in front of door
-                console.log("nospawn");
                 m[i][j] = 0;
                 continue;
             }
