bace61551b3d7654a808c003d5c8b7dfcd008f32
Steven Branson <steve@diodeware.com>
2018-03-02 16:44:20 -0600

    Add form to input your own data

--- d3_scatter_plot/app.js
+++ d3_scatter_plot/app.js
@@ -58,7 +58,7 @@ function render(data) {
     // pad the domain so the data isnt on top of the axis
     yExtent[1] += 1;
     xExtent[1] += 10;
-    
+
     scaleY.domain(yExtent);
     scaleX.domain(xExtent);
 
@@ -77,16 +77,18 @@ function render(data) {
 
     point.merge(enterPoint)
         .attr("transform", (d, i) => `translate(${scaleX(d["Seconds"] - firstPlaceSeconds)}, ${scaleY(d["Place"])})`)
-        .on("mouseover", d => infobox.style("visibility", "visible").html(infoboxFormat(d)));
+        .on("mouseover", (d) => { infobox.style("visibility", "visible").html(infoboxFormat(d)); });
 
     point.merge(enterPoint)
-        .selectAll(".point-circle")
-          .attr("r", circleSize)
-          .attr("fill", (d, i) => (d["Doping"] === "") ? mutedGreen : mutedRed );
+        .select(".point-name")
+            .text((d, i) => d["Name"]);
 
     point.merge(enterPoint)
-        .selectAll(".point-name")
-          .text((d, i) => d["Name"]);
+        .select(".point-circle")
+            .attr("r", circleSize)
+            .attr("fill", (d, i) => (d["Doping"] === "") ? mutedGreen : mutedRed );
+
+
 
 
     let yAxis = d3.axisLeft(scaleY);
@@ -128,3 +130,31 @@ d3.json("cyclists-data.json", (e, d) => {
         .attr("transform", `translate(0, ${height})`);
     render(d);
 });
+
+function addData() {
+    globalData.push(objectifyForm());
+    globalData = rerankData(globalData);
+    render(globalData);
+}
+
+function objectifyForm() {
+    let obj = {
+        "Name": document.getElementById("name-input").value,
+        "Seconds": parseFloat(document.getElementById("time-input").value),
+        "URL": document.getElementById("doping-source").value,
+        "Doping": document.getElementById("doping-description").value
+    };
+    let inputs = document.getElementsByTagName("input");
+    for(let i = 0; i < inputs.length; i++) {
+        inputs[i].value = "";
+    }
+    return obj;
+}
+
+function rerankData(data) {
+    data.sort((a, b) => a["Seconds"] < b["Seconds"] ? -1 : +1);
+    for(i = 0; i < data.length; i++) {
+        data[i]["Place"] = i+1;
+    }
+    return data;
+}
