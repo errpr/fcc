f3faad07517bc3402b2e721c2cc6f38f9199a47f
Steven Branson <steve@diodeware.com>
2018-03-03 20:18:04 -0600

    Size dots according to mass

--- d3_meteor_map/app.js
+++ d3_meteor_map/app.js
@@ -4,7 +4,6 @@ const margin = { top: 50, right: 50, bottom: 50, left: 50 };
 
 const outerWidth = 800;
 const outerHeight = 600;
-const circleSize = 32;
 
 const width = outerWidth - margin.left - margin.right;
 const height = outerHeight - margin.top - margin.bottom;
@@ -16,32 +15,38 @@ let svg = d3.select("#root")
                 .call(d3.zoom().on("zoom", () => chart.attr("transform", d3.event.transform) ));
 let chart = svg.append("g")
                 .attr("transform", `translate(${margin.left}, ${margin.top})`)
-                
-
-let layout = d3.forceSimulation();
 
 let infobox = d3.select("#infobox").style("visibility", "hidden");
 
 function infoboxFormat(data) {
-    return `<p>${data["country"]}</p>`;
+    return `<p>${data["name"]}</p>
+            <p>Year: ${new Date(Date.parse(data["year"])).getFullYear()}</p>
+            <p>Mass: ${data["mass"]}</p>`;
 }
 
 function render(geoData, meteorData) {
-    let projection = d3.geoMercator().scale(500).translate([width / 2, height / 2])
+    let projection = d3.geoMercator().scale(1000).translate([width / 2, height / 2])
 
     let geoPath = d3.geoPath().projection(projection);
     let map = chart.append("path").datum(geoData)
         .attr("d", geoPath)
-        .attr("fill", "#aaa")
+        .attr("fill", "#333")
         .attr("stroke", "#fff");
-
+    let scaleExtent = d3.extent(meteorData.features.reduce((a, e) => { a.push(+e["properties"]["mass"]); return a }, []));
+    scaleExtent[1] /= 4; // some huge meteor nuked vladivostok, its throwing the circle size off
+    let scaleMass = d3.scaleLinear()
+        .range([3, 200])
+        .domain(scaleExtent)
+        .clamp(true);
+    let geoPath2 = d3.geoPath().projection(projection).pointRadius(d => scaleMass(d["properties"]["mass"]))
     let strikes = chart.selectAll(".meteorPath")
         .data(meteorData.features)
         .enter()
         .append("path")
-        .attr("class", "meteorPath")
-        .attr("d", geoPath)
-        .attr("fill", d3.hsl(120, 0.5, 0.5))
+            .attr("class", "meteorPath")
+            .attr("d", geoPath2)
+            .attr("fill", d3.hsl(120, 0.5, 0.5))
+            .style("width", 50)
 }
 
 d3.json("custom.geo.json", (e, d) => {
