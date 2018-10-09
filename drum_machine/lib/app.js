e4f5e05fc0a00ab0cdf9859eb4a757ef40c8ef8b
Steven B <steve@diodeware.com>
2018-10-09 11:29:11 -0500

    More like dumb machine

--- drum_machine/lib/app.js
+++ drum_machine/lib/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -8,27 +8,50 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+function DrumPad(props) {
+	return React.createElement(
+		"div",
+		{ id: "pad-" + props.padKey,
+			className: "drum-pad",
+			onclick: props.clickPad },
+		props.padKey,
+		React.createElement(
+			"audio",
+			{
+				id: props.padKey,
+				src: props.padKey + ".mp3" },
+			"Your browser does not support the ",
+			React.createElement(
+				"code",
+				null,
+				"audio"
+			),
+			" element."
+		)
+	);
+}
+
 var App = function (_React$Component) {
-    _inherits(App, _React$Component);
+	_inherits(App, _React$Component);
 
-    function App() {
-        _classCallCheck(this, App);
+	function App() {
+		_classCallCheck(this, App);
 
-        return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
-    }
+		return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+	}
 
-    _createClass(App, [{
-        key: 'render',
-        value: function render() {
-            return React.createElement(
-                'h1',
-                null,
-                'App'
-            );
-        }
-    }]);
+	_createClass(App, [{
+		key: "render",
+		value: function render() {
+			return React.createElement(
+				"h1",
+				null,
+				"App"
+			);
+		}
+	}]);
 
-    return App;
+	return App;
 }(React.Component);
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
+ReactDOM.render(React.createElement(App, null), document.getElementById('drum-machine'));
--- drum_machine/src/app.jsx
+++ drum_machine/src/app.jsx
@@ -1,6 +1,20 @@
+function DrumPad(props) {
+	return (
+		<div id={"pad-" + props.padKey} 
+			className="drum-pad" 
+			onclick={props.clickPad}>
+			{props.padKey}
+			<audio
+        		id={props.padKey}
+        		src={props.padKey + ".mp3"}>
+        		Your browser does not support the <code>audio</code> element.
+    		</audio>
+		</div>
+	);
+}
 class App extends React.Component {
     render() {
         return(<h1>App</h1>);
     }
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('drum-machine'));
