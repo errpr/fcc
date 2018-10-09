f8c60149c4959b60c4bf538c26a4f75478cf6639
Steven Branson <steve@diodeware.com>
2018-10-09 15:54:14 -0500

    Finish drum_machine

--- drum_machine/lib/app.js
+++ drum_machine/lib/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -8,45 +8,109 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var AUDIO_HOST_URL = "http://error.diodeware.com/fcc/drum_machine/audio/";
+var PAD_KEYS = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+
 function DrumPad(props) {
 	return React.createElement(
-		"div",
+		'div',
 		{ id: "pad-" + props.padKey,
-			className: "drum-pad",
-			onclick: props.clickPad },
+			className: 'drum-pad',
+			onClick: props.onClick },
 		props.padKey,
 		React.createElement(
-			"audio",
+			'audio',
 			{
 				id: props.padKey,
-				src: props.padKey + ".mp3" },
-			"Your browser does not support the ",
+				src: AUDIO_HOST_URL + props.padKey + ".wav",
+				preload: 'auto',
+				className: 'clip',
+				loop: false },
+			'Your browser does not support the ',
 			React.createElement(
-				"code",
+				'code',
 				null,
-				"audio"
+				'audio'
 			),
-			" element."
+			' element.'
 		)
+	);
+}
+
+function DrumPadGrid(props) {
+	var drumPads = PAD_KEYS.map(function (e) {
+		return React.createElement(DrumPad, { key: "pad-" + e, padKey: e, onClick: props.createClickHandler(e) });
+	});
+
+	return React.createElement(
+		'div',
+		{ id: 'drum-pad-grid' },
+		drumPads
+	);
+}
+
+function DrumDisplay(props) {
+	return React.createElement(
+		'div',
+		{ id: 'display' },
+		props.text
 	);
 }
 
 var App = function (_React$Component) {
 	_inherits(App, _React$Component);
 
-	function App() {
+	function App(props) {
 		_classCallCheck(this, App);
 
-		return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+		var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+
+		_this.state = {
+			currentButton: '\0'
+		};
+
+		_this.playSample = function (key) {
+			_this.setState({ currentButton: key });
+			var audioElement = document.getElementById(key);
+			audioElement.pause();
+			audioElement.currentTime = 0;
+			audioElement.play();
+		};
+
+		_this.handleKeys = function (event) {
+			var k = event.key.toUpperCase();
+			if (k && PAD_KEYS.includes(k)) {
+				_this.playSample(k);
+			}
+		};
+
+		_this.createClickHandler = function (keyCode) {
+			var root = _this;
+			return function () {
+				root.playSample(keyCode);
+			};
+		};
+		return _this;
 	}
 
 	_createClass(App, [{
-		key: "render",
+		key: 'componentDidMount',
+		value: function componentDidMount() {
+			window.addEventListener("keydown", this.handleKeys);
+		}
+	}, {
+		key: 'componentWillUnmount',
+		value: function componentWillUnmount() {
+			window.removeEventListener("keydown", this.handleKeys);
+		}
+	}, {
+		key: 'render',
 		value: function render() {
 			return React.createElement(
-				"h1",
-				null,
-				"App"
+				'div',
+				{ id: 'drum-machine' },
+				React.createElement(DrumDisplay, { text: this.state.currentButton }),
+				React.createElement(DrumPadGrid, { createClickHandler: this.createClickHandler })
 			);
 		}
 	}]);
@@ -54,4 +118,4 @@ var App = function (_React$Component) {
 	return App;
 }(React.Component);
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('drum-machine'));
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
--- drum_machine/src/app.jsx
+++ drum_machine/src/app.jsx
@@ -1,20 +1,90 @@
+const AUDIO_HOST_URL = "http://error.diodeware.com/fcc/drum_machine/audio/";
+const PAD_KEYS = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+
 function DrumPad(props) {
 	return (
 		<div id={"pad-" + props.padKey} 
 			className="drum-pad" 
-			onclick={props.clickPad}>
+			onClick={props.onClick}>
 			{props.padKey}
 			<audio
         		id={props.padKey}
-        		src={props.padKey + ".mp3"}>
+        		src={AUDIO_HOST_URL + props.padKey + ".wav"}
+				preload="auto"
+				className="clip"
+				loop={false}>
         		Your browser does not support the <code>audio</code> element.
     		</audio>
 		</div>
 	);
 }
+
+function DrumPadGrid(props) {
+	const drumPads = PAD_KEYS.map(e => {
+		return <DrumPad key={"pad-" + e} padKey={e} onClick={props.createClickHandler(e)} />;
+	});
+
+	return (
+		<div id="drum-pad-grid">
+			{ drumPads }
+		</div>
+	);
+}
+
+function DrumDisplay(props) {
+	return(
+		<div id="display">
+			{props.text}
+		</div>
+	);
+}
+
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			currentButton: "\u0000"
+		}
+
+		this.playSample = (key) => {
+			this.setState({ currentButton: key });
+			let audioElement = document.getElementById(key);
+			audioElement.pause();
+			audioElement.currentTime = 0;
+			audioElement.play();
+		}
+
+		this.handleKeys = (event) => {
+			const k = event.key.toUpperCase();
+			if (k && PAD_KEYS.includes(k)) {
+				this.playSample(k);
+			}
+		}
+
+		this.createClickHandler = (keyCode) => {
+			const root = this;
+			return function() {
+				root.playSample(keyCode);
+			}
+		}
+	}
+
+	componentDidMount() {
+		window.addEventListener("keydown", this.handleKeys);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("keydown", this.handleKeys);
+	}
+
     render() {
-        return(<h1>App</h1>);
+        return(
+			<div id="drum-machine">
+				<DrumDisplay text={this.state.currentButton} />
+				<DrumPadGrid createClickHandler={this.createClickHandler} />
+			</div>
+		);
     }
 }
-ReactDOM.render(<App />, document.getElementById('drum-machine'));
+ReactDOM.render(<App />, document.getElementById('root'));
